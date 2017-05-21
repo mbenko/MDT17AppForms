09f76cc28b465f538a31b246ee6931780c2f601a
Ima Dev <msdn@benko.com>
2017-05-20 19:45:48 -0500

    Add core content

---
+++ CoreWebApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace CoreWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            var client = new HttpClient();
+            var url = MySettings.API_URL + "/api/values";
+
+            var response = await client.GetAsync(url);
+
+            var json = response.Content.ReadAsStringAsync();
+
+            ViewData["Message"] = $"Called API - {json.Result}";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoreWebApp/MySettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApp
+{
+    public class MySettings
+    {
+        public static string API_URL { get; set; }
+    }    
+}
